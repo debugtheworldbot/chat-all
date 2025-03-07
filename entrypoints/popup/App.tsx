eb1e2e0077628902f28324e8afb83c0ef4a058d5
tian <istiancz@gmail.com>
2025-03-07 20:58:57 +0800

    fix: style

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -3,22 +3,16 @@ import './App.css'
 function App() {
 	return (
 		<>
-			<h1>WXT + React</h1>
+			<h1>ChatGPT, Claude, Grok all in one</h1>
 			<div className='card'>
 				<button
 					onClick={() => {
 						chrome.tabs.create({ url: chrome.runtime.getURL('home.html') })
 					}}
 				>
-					home
+					OPEN
 				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
 			</div>
-			<p className='read-the-docs'>
-				Click on the WXT and React logos to learn more
-			</p>
 		</>
 	)
 }
